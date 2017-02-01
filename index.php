0f1c6543971038f80b813bb1cf592a4cfcc31e6c
David <d.kington7710@gmail.com>
2017-02-01 13:28:40 -0600

    added comment

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>David's Website</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+<body>
+<header>
+    <h1>
+David's Website
+    </h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="/">Homepage<a/></li>
+        <li><a href="#">Loop</a></li>
+        <li><a href="#">Countdown</a></li>
+    </ul>
+</nav>
+<main>
+    <img src="images/David.jpg" alt="Picture of David"/>
+    <p> This is my 3rd semester of the Software Development program and this is work of my PHP class. </p>
+</main>
+<footer>
+    &copy; 2017 David Kington
+</footer>
+</body>
+</html>
